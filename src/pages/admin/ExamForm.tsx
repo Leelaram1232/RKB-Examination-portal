a4eb33e3c50e9a2ca2af8cd34f3a863abd7b379b
leelaram1232 <bjram04@gmail.com>
2026-05-08 10:43:17 +0530

    Fix JSX tagging error in ExamForm.tsx

--- src/pages/admin/ExamForm.tsx
+++ src/pages/admin/ExamForm.tsx
@@ -539,7 +539,7 @@ const ExamForm = () => {
                       />
                     </div>
 
-                    </div>
+
 
                     <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
                       <FormField
